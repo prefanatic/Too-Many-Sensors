d35e71943f264581958484fa30db85d41339929d
Cody Goldberg <dumbplanet424@gmail.com>
2016-01-13 13:15:46 -0500

    Fixed crash on API < 23.

--- mobile/src/main/java/io/github/prefanatic/toomanysensors/ui/NavigationActivity.kt
+++ mobile/src/main/java/io/github/prefanatic/toomanysensors/ui/NavigationActivity.kt
@@ -18,6 +18,7 @@ package io.github.prefanatic.toomanysensors.ui
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
@@ -43,7 +44,7 @@ class NavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
         setSupportActionBar(toolbar)
-        setStatusBarColor(resources.getColor(R.color.colorPrimaryDark, theme))
+        setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
 
         with (supportActionBar) {
             setHomeAsUpIndicator(R.drawable.ic_menu_24dp)
