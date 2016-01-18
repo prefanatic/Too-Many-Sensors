e8318e221163e1b706e0ffb4215fed87d3d68faf
Cody Goldberg <dumbplanet424@gmail.com>
2016-01-18 10:38:15 -0500

    Fixed crash on API < 23 in recording sensor data.

--- mobile/src/main/java/io/github/prefanatic/toomanysensors/adapter/SensorDataAdapter.kt
+++ mobile/src/main/java/io/github/prefanatic/toomanysensors/adapter/SensorDataAdapter.kt
@@ -17,6 +17,7 @@
 package io.github.prefanatic.toomanysensors.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -83,7 +84,7 @@ class SensorDataAdapter(val context: Context) : RecyclerView.Adapter<SensorDataA
         }
 
         val dataSet = BarDataSet(yValues, "DataSet")
-        dataSet.color = context.getColor(R.color.colorAccent)
+        dataSet.color = ContextCompat.getColor(context, R.color.colorAccent)
 
         return BarData(xValues, dataSet)
     }
